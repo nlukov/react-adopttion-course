0285f106378bd0c72643daa5a6d194c7ae70394f
Nikolay Lukov <nikolay.lukov@trivago.com>
2019-05-21 13:27:49 +0200

    Added Details page, with Class, loading, and details about the animal

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,51 @@
 import React from "react";
 import { Router } from "@reach/router";
+import pet from "@frontendmasters/pet";
 
-const Details = props => {
-  return (
-    <pre>
-      <code>{JSON.stringify(props, null, 4)}</code>
-    </pre>
-  );
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true
+    };
+  }
+  //works like ajax request
+  componentDidMount() {
+    pet
+      .animal(this.props.id) //this is IMMUTABLE, it can't change them, only reads
+      .then(({ animal }) => {
+        this.setState({
+          name: animal.name,
+          animal: animal.type,
+          location: `${animal.contact.address.city}, ${
+            animal.contact.address.state
+          }`,
+          description: animal.description,
+          media: animal.photos,
+          breed: animal.breeds.primary,
+          loading: false
+        });
+      }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
