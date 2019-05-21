26efab3917d7e373c34b45b5ef2a135d78729ade
Nikolay Lukov <nikolay.lukov@trivago.com>
2019-05-21 09:39:04 +0200

    Wrap the app in React.StrictMode

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
 
+//use of React.StrictMode. This will throw warnings for things that the React Team will depricate.
+//It is recommended to use this for new applciations and NOT for legacy!
 const App = () => {
   return (
-    <div>
-      <h1 id="something-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+      <div>
+        <h1 id="something-important">Adopt Me!</h1>
+        <SearchParams />
+      </div>
+    </React.StrictMode>
   );
 };
 
