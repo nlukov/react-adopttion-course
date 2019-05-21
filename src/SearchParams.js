96cd9ac54cce41faf5f27aca685cd34f6c558887
Nikolay Lukov <nikolay.lukov@trivago.com>
2019-05-21 10:24:56 +0200

    Create asynch function on submit. Returns array of selected animal and breed and region

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,12 +1,27 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
+import { async } from "q";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA"); // this is a Hook. All hooks begin with 'use'
   const [breeds, setBreeds] = useState([]);
   const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS); //using the custom hook
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds); //using the custom hook
+  const [pets, setPets] = useState([]);
+
+  //asynch function is guaranteed to return a promise.
+  async function requestPets() {
+    const { animals } = await pet.animals({
+      location,
+      breed,
+      type: animal
+    });
+
+    setPets(animals || []);
+
+    console.log(animals);
+  }
 
   // useEffect takes place of several life cycle hooks (componentDidMount, componentDidUpdate, componentWillMlount)
   // The way it works is it makes a call after render time. As soon as SearchParams renders the first time, then useEffect runs.
@@ -22,7 +37,12 @@ const SearchParams = () => {
 
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
